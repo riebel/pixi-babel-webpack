b166e792195dada63e56f4176b6af138a856aaa8
Hagen Sommerkorn <hagen.sommerkorn@gmail.com>
2017-04-15 23:20:17 +0200

    updated to webpack2

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,16 +1,8 @@
 import loaders from './webpack.loaders';
-var path = require('path');
+const path = require('path');
 
-const namedArgs = process.argv.reduce((o, v) => {
-    const arg = v.split('=');
-    o[arg[0]] = arg[1] || true;
-    return o;
-}, {});
-
-const useServerPath = '--useServerPath' in namedArgs;
 const DEV_SERVER_PORT = 3000;
 const REL_PATH = '/assets/';
-const PUBLIC_PATH = useServerPath ? `http://localhost:${DEV_SERVER_PORT}${REL_PATH}` : REL_PATH;
 const BUILD_PATH = path.resolve(__dirname, `web${REL_PATH}`);
 
 module.exports = {
@@ -18,18 +10,14 @@ module.exports = {
         './src/app'
     ],
     devServer: {
-        devtool: 'source-map',
-        hot: true,
-        inline: true,
         port: DEV_SERVER_PORT,
-        outputPath: BUILD_PATH,
         headers: {
             'Access-Control-Allow-Origin': '*'
         },
         contentBase: 'web/'
     },
     output: {
-        publicPath: PUBLIC_PATH,
+        publicPath: REL_PATH,
         path: BUILD_PATH,
         filename: '[name].bundle.js',
         chunkFilename: '[id].js'
@@ -37,6 +25,5 @@ module.exports = {
     devtool: 'source-map',
     module: {
         loaders
-    },
-    debug: true
+    }
 };
